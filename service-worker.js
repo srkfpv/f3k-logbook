6a32b79f650a2652d764e88b73ae2048b5c7044b
SRKF3K <siarkowski@gmail.com>
2026-05-25 08:26:23 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='f3k-logbook-v3';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg','./logs/index.csv'];
+const CACHE='f3k-logbook-v14-pink';
+const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./favicon-32.png','./logs/index.csv'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
