d5536b1be6a9ab8f9929c16ef1c9a0f5ddd064de
srkfpv <siarkowski@gmail.com>
2026-05-28 09:43:26 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
-const APP_BUILD = '58.0';
+const APP_BUILD = '59.0';
 const LOG_DIR = 'logs/';
-const CACHE_BUST = 'v58-' + Date.now();
+const CACHE_BUST = 'v59-' + Date.now();
 
 const $ = id => document.getElementById(id);
 const canvas = $('chartCanvas');
